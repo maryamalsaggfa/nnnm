2e652fe4b3641ab3feb793c76dc8540f2aaa6d78
maryamalsaggfa <maryam1xsa@gmail.com>
2023-10-07 14:48:22 -0700

    Initial Commit

---
+++ nnnm/nnnmApp.swift
@@ -0,0 +1,17 @@
+//
+//  nnnmApp.swift
+//  nnnm
+//
+//  Created by maryam on 22/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct nnnmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
